4c651411364032b5b49303397007d65d9de2a7b5
Aidan Hill <aidan.hill@philips.com>
2020-08-04 10:45:43 -0400

    updating home page

--- BakerySquared/App_Start/RouteConfig.cs
+++ BakerySquared/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BakerySquared
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Floor1", id = UrlParameter.Optional }
             );
         }
     }
