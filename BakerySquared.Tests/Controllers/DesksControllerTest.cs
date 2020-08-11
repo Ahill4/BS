70524bfc3a50722eb2134be965e15aec6815e0a2
Matt Theys <matt.theys@philips.com>
2020-08-11 08:59:31 -0400

    Revert "deleted App_Data files"
    This reverts commit c7fdcc31f0e6f715dc6928d114b7ea293fe48e84.

---
+++ BakerySquared.Tests/Controllers/DesksControllerTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using BakerySquared.Models;
+using BakerySquared.Controllers;
+using System.Web.Mvc;
+using BSDB.Models;
+using System.Linq;
+using PagedList;
+
+namespace BakerySquared.Tests.Controllers
+{
+    /// <summary>
+    /// Test class for DeskController.cs and it's methods.
+    /// </summary>
+    [TestClass]
+    public class DesksControllerTest
+    {
+        /// <summary>
+        /// Tests that the Index() method produces a paged list of Desk objects in its view.
+        /// </summary>
+        [TestMethod]
+        public void Index_ViewContains_PagedListOfDesks()
+        {
+            // Arrange
+            Mock<IDesksRepository> mock = new Mock<IDesksRepository>();
+
+            mock.Setup(d => d.ToQuery()).Returns(new Desk[]
+            {
+                new Desk {  Desk_Id = "D1000", Occupant = "Occupant1"   },
+                new Desk {  Desk_Id = "D1001", Occupant = "Occupant2"   },
+                new Desk {  Desk_Id = "D1002", Occupant = "Occupant3"   }
+            }.AsQueryable());
+
+            DesksController controller = new DesksController(mock.Object);
+
+            // Act
+            var actual = (PagedList<Desk>)controller.Index("", "", "", 1).Model;
+            
+            // Assert
+            Assert.IsInstanceOfType(actual, typeof(PagedList<Desk>));
+        }
+
+        /// <summary>
+        /// Tests that the Details() method produces a Desk object in it's view.
+        /// </summary>
+        [TestMethod]
+        public void Details_ViewContains_DeskObject()
+        {
+            // Arrange
+            Mock<IDesksRepository> mock = new Mock<IDesksRepository>();
+
+            mock.Setup(d => d.Find("")).Returns(new Desk { Desk_Id = "D1000", Occupant = "Occupant1" });
+
+            DesksController controller = new DesksController(mock.Object);
+
+            // Act
+            var actual = (Desk)controller.Details("").Model;
+
+            // Assert
+            Assert.IsInstanceOfType(actual, typeof(Desk));
+        }
+
+        /// <summary>
+        /// Tests that the Create() method, when called to create a desk that does not already
+        /// exist in the database, redirects to a different action ("Index" action).
+        /// </summary>
+        [TestMethod]
+        public void Create_DeskDoesNotExist_RedirectsToAction()
+        {
+            // Arrange
+            Mock<IDesksRepository> mock = new Mock<IDesksRepository>();
+
+            mock.Setup(d => d.AlreadyExists(new Desk())).Returns(false);
+
+            DesksController controller = new DesksController(mock.Object);
+
+            // Act
+            var actual = controller.Create(new Desk());
+
+            // Assert
+            Assert.IsInstanceOfType(actual, typeof(RedirectToRouteResult));
+        }
+
+        /// <summary>
+        /// Tests that the Create() method, when called to create a desk that already exists
+        /// in the database, returns an Action Result (View Result with attempted Desk_Id and Occupant).
+        /// </summary>
+        [TestMethod]
+        public void Create_DeskAlreadyExist_ViewContainsDeskObject()
+        {
+            // Arrange
+            Mock<IDesksRepository> mock = new Mock<IDesksRepository>();
+
+            mock.Setup(d => d.AlreadyExists(new Desk())).Returns(true);
+
+            DesksController controller = new DesksController(mock.Object);
+
+            // Act
+            var actual = controller.Create(new Desk());
+
+            // Assert
+            Assert.IsInstanceOfType(actual, typeof(ActionResult));
+        }
+    }
+
+}
---
+++ BakerySquared/Models/EFAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace BakerySquared.Models
+{
+    public class EFAccountRepository : IAccountRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();        
+
+        public IEnumerable<ApplicationUser> ToList()
+        {
+            return db.Users.ToList();
+        }
+    }
+}
