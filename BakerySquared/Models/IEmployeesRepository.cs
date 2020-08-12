f3738a469e6ae3d3b9f76304a81d4321daed706a
matt.theys <matt.theys@philips.com>
2020-08-12 13:13:51 -0400

    added class and interface for db abstraction for unit testing

---
+++ BakerySquared/Models/IEmployeesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BakerySquared.Models;
+using BSDB.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Linq;
+
+namespace BakerySquared.Models
+{
+    public class IEmployeesRepository
+    {
+    }
+}
